74bc84ddf21168202ac31fa4799b7fbfbc5d4bb0
Максим Якуб <79774488441@ya.ru>
2022-11-10 05:13:47 +0000

    Resolve "Разработать страницу "участники""

--- src/main/java/com/javamentor/qa/platform/webapp/configs/WebConfig.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/WebConfig.java
@@ -10,5 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/regpage").setViewName("regpage");
+        registry.addViewController("/usersPage").setViewName("usersPage");
     }
 }
