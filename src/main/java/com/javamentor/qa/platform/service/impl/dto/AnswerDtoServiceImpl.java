37a7008537eae4d83fe15b13f129beddb3bdb29f
Vladislav Panov <noobik41y@gmail.com>
2022-11-21 09:27:43 +0000

    Resolve "Разработать api получения списка DTO ответов по id вопроса #18"

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/AnswerDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/AnswerDtoServiceImpl.java
@@ -1,9 +1,25 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
+import com.javamentor.qa.platform.dao.abstracts.dto.AnswerDtoDao;
+import com.javamentor.qa.platform.models.dto.AnswerDto;
 import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class AnswerDtoServiceImpl implements AnswerDtoService {
 
+    private final AnswerDtoDao answerDtoDao;
+
+    public AnswerDtoServiceImpl(AnswerDtoDao answerDtoDao) {
+        this.answerDtoDao = answerDtoDao;
+    }
+
+    @Override
+    @Transactional
+    public List<AnswerDto> getAllAnswersDtoByQuestionId(Long questionId, Long userId) {
+        return answerDtoDao.getAllAnswersDtoByQuestionId(questionId, userId);
+    }
 }
