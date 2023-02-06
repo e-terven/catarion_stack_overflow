85f59d079899c459d71ad7e805be51e74fe29cfc
Дмитрий Мирошников <mirflames@yandex.ru>
2023-02-06 13:32:56 +0000

    Resolve "Реализовать api, которое позволяет добавить комментарий к ответу на вопрос #24"

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/CommentAnswerService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/CommentAnswerService.java
@@ -1,7 +1,11 @@
 package com.javamentor.qa.platform.service.abstracts.model;
 
+import com.javamentor.qa.platform.models.dto.CommentAnswerDto;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.question.answer.CommentAnswer;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.repository.ReadWriteService;
 
 public interface CommentAnswerService extends ReadWriteService<CommentAnswer, Long> {
+    CommentAnswerDto addCommentToAnswer(User user, Answer answer, String text);
 }
--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,33 +1,49 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.models.dto.AnswerDTO;
+import com.javamentor.qa.platform.models.dto.CommentAnswerDto;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
+import com.javamentor.qa.platform.service.abstracts.dto.CommentAnswerDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
+import com.javamentor.qa.platform.service.abstracts.model.CommentAnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
+import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/user/question/{questionId}/answer")
+@Tag(name = "API ответа на вопрос", description = "Эндпойнты для работы с ответами на вопросы")
 public class ResourceAnswerController {
 
     private final AnswerDtoService answerDtoService;
     private final QuestionService questionService;
+    private final CommentAnswerService commentAnswerService;
+    private final UserService userService;
+    private final AnswerService answerService;
+    private final CommentAnswerDtoService commentAnswerDtoService;
 
     public ResourceAnswerController(AnswerDtoService answerDtoService,
-                                    QuestionService questionService) {
+                                    QuestionService questionService,
+                                    CommentAnswerService commentAnswerService,
+                                    UserService userService,
+                                    AnswerService answerService,
+                                    CommentAnswerDtoService commentAnswerDtoService) {
         this.answerDtoService = answerDtoService;
         this.questionService = questionService;
+        this.commentAnswerService = commentAnswerService;
+        this.userService = userService;
+        this.answerService = answerService;
+        this.commentAnswerDtoService = commentAnswerDtoService;
     }
 
     @GetMapping
@@ -40,5 +56,23 @@ public class ResourceAnswerController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(answerDtoService.getAllAnswersDtoByQuestionId(questionId, user.getId()), HttpStatus.OK);
+    }
+
+    @PostMapping("{answerId}/comment")
+    @Operation(description = "Добавляет новый комментарий к ответу")
+    @ApiResponse(responseCode = "200", description = "успешно")
+    @ApiResponse(responseCode = "400", description = "Ответа по ID не существует")
+    public ResponseEntity<CommentAnswerDto> commentAnswer(@AuthenticationPrincipal User user,
+                                                          @PathVariable String questionId,
+                                                          @PathVariable Long answerId,
+                                                          @RequestBody String text) {
+
+        if (text.isBlank()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else if (!answerService.existsById(answerId)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(commentAnswerService.addCommentToAnswer(userService.getById(user.getId()).get(),
+                answerService.getById(answerId).get(), text), HttpStatus.OK);
     }
 }
