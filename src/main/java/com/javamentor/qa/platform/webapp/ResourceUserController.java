fe36c6d620da9d6a7d1165866cc468dd2b14c358
Alexander Tyutin <a.dencyh@gmail.com>
2023-03-06 08:05:07 +0000

    Resolve "Разработать api, который возвращает пользователя по его id #17"

---
+++ src/main/java/com/javamentor/qa/platform/webapp/ResourceUserController.java
@@ -0,0 +1,40 @@
+package com.javamentor.qa.platform.webapp;
+
+import com.javamentor.qa.platform.models.dto.UserDto;
+import com.javamentor.qa.platform.service.abstracts.dto.UserDtoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/user")
+@AllArgsConstructor
+public class ResourceUserController {
+
+	private final UserDtoService userDtoService;
+
+	@ApiOperation(
+		value = "Получение пользователя по id",
+		response = UserDto.class,
+		notes = "Возвращает объект UserDto"
+	)
+	@ApiResponses(
+		value = {
+			@ApiResponse(code = 200, message = "Пользователь получен"),
+			@ApiResponse(code = 404, message = "Пользователь не найден")
+		}
+	)
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDto> getUserDtoById(@PathVariable("id") Long id) {
+		Optional<UserDto> userDtoOptional = userDtoService.getById(id);
+		return userDtoOptional.isPresent()
+			   ? new ResponseEntity<>(userDtoOptional.get(), HttpStatus.OK)
+			   : new ResponseEntity<>(new UserDto(), HttpStatus.NOT_FOUND);
+	}
+}
