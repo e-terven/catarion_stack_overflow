7e6b4ca2b61ce38f36d4b935ef188cdc24df50a7
Anastasiia Milashevich <anastasmilash@gmail.com>
2023-03-15 12:53:52 +0000

    Resolve "Разработать api которое считает количество вопросов в базе #40"

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/QuestionDaoImpl.java
@@ -2,11 +2,13 @@ package com.javamentor.qa.platform.dao.impl.model;
 
 import com.javamentor.qa.platform.dao.abstracts.model.QuestionDao;
 import com.javamentor.qa.platform.dao.impl.repository.ReadWriteDaoImpl;
+import com.javamentor.qa.platform.dao.util.SingleResultUtil;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 @Repository
 public class QuestionDaoImpl extends ReadWriteDaoImpl<Question, Long> implements QuestionDao {
@@ -14,4 +16,14 @@ public class QuestionDaoImpl extends ReadWriteDaoImpl<Question, Long> implements
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Override
+    public Optional<Long> getCountQuestion() {
+        return SingleResultUtil.getSingleResultOrNull(entityManager.createQuery(
+                """
+                        select count (q)
+                        from Question q
+                        where q.isDeleted = FALSE
+                        """, Long.class
+        ));
+    }
 }
--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
@@ -3,5 +3,8 @@ package com.javamentor.qa.platform.service.abstracts.model;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.service.abstracts.repository.ReadWriteService;
 
+import java.util.Optional;
+
 public interface QuestionService extends ReadWriteService<Question, Long> {
+    Optional<Long> getCountQuestion();
 }
