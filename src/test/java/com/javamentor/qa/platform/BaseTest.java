88af3499dd0b22db439b78256deb5da163d1a8a6
iTim555 <timur555@yandex.ru>
2023-03-16 17:13:53 +0300

    ref

--- src/test/java/com/javamentor/qa/platform/BaseTest.java
+++ src/test/java/com/javamentor/qa/platform/BaseTest.java
@@ -1,14 +1,18 @@
 package com.javamentor.qa.platform;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.junit5.api.DBRider;
 import com.javamentor.qa.platform.models.dto.AuthenticationRequestDto;
-import com.javamentor.qa.platform.models.dto.TokenResponseDto;
 import com.javamentor.qa.platform.webapp.configs.JmApplication;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -16,39 +20,37 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-import javax.transaction.Transactional;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = JmApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application.properties")
 @AutoConfigureMockMvc
-@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestEntityManager
 @DBRider
 @DBUnit(caseInsensitiveStrategy = Orthography.LOWERCASE)
 public abstract class BaseTest {
 
-	@Autowired
-	protected MockMvc mockMvc;
+    @Autowired
+    protected MockMvc mockMvc;
+    protected final ObjectMapper objectMapper = new ObjectMapper();
 
-	protected final ObjectMapper objectMapper = new ObjectMapper();
+    {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
 
-	public String getToken(String username, String password) throws Exception {
-		AuthenticationRequestDto requestDto = new AuthenticationRequestDto();
-		requestDto.setLogin(username);
-		requestDto.setPass(password);
+    public String getToken(String username, String password) throws Exception {
+        AuthenticationRequestDto requestDto = new AuthenticationRequestDto(username, password);
+        String json = objectMapper.writeValueAsString(requestDto);
 
-		MvcResult result = mockMvc
-			.perform(post("/api/auth/token")
-						 .contentType(MediaType.APPLICATION_JSON)
-						 .content(objectMapper.writeValueAsString(requestDto)))
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andReturn();
-		String responseBody = result.getResponse().getContentAsString();
-		TokenResponseDto tokenResponseDto = objectMapper.readValue(responseBody, TokenResponseDto.class);
-		return "Bearer " + tokenResponseDto.getToken();
-	}
+        MvcResult result = mockMvc
+                .perform(post("/api/auth/token").contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+        return "Bearer " + JsonPath.read(result.getResponse().getContentAsString(), "token");
+    }
 }
