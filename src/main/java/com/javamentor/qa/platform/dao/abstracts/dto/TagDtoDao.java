62d2f11a1a28e2210b6a6492aa5c63551b15df0f
Anastasiia Milashevich <anastasmilash@gmail.com>
2023-03-16 14:49:49 +0000

    Resolve "Разработать api который возвращает лист, содержащий топ-10 тегов #42"

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/TagDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/TagDtoDao.java
@@ -1,4 +1,11 @@
 package com.javamentor.qa.platform.dao.abstracts.dto;
 
+import com.javamentor.qa.platform.models.dto.RelatedTagDto;
+
+import java.util.List;
+
 public interface TagDtoDao {
+
+    List<RelatedTagDto> getTopTags();
+
 }
--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/TagDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/TagDtoService.java
@@ -1,6 +1,11 @@
 package com.javamentor.qa.platform.service.abstracts.dto;
 
+import com.javamentor.qa.platform.models.dto.RelatedTagDto;
+
+import java.util.List;
+
 public interface TagDtoService {
 
+    List<RelatedTagDto> getTopTags();
 
 }
