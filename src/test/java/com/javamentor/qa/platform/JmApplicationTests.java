c6d06f7e675e319fe3f276ee5f92660cdf9e5927
Vlad Panin <vlad.frotello@gmail.com>
2023-04-17 08:48:33 +0000

    Resolve "Разделение на 2 профиля"

--- src/test/java/com/javamentor/qa/platform/JmApplicationTests.java
+++ src/test/java/com/javamentor/qa/platform/JmApplicationTests.java
@@ -1,14 +1,13 @@
 package com.javamentor.qa.platform;
 
+
 import com.javamentor.qa.platform.webapp.configs.JmApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = JmApplication.class)
 class JmApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
