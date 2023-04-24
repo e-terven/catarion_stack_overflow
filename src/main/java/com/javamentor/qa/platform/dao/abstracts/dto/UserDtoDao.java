8801ee9825893ddbd255a4ea6575a753269a6600
Amargenter <amargenter@gmail.com>
2023-04-24 12:20:30 +0000

    Resolve "Разработать api, который возвращает пользователя по его id #17"

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/UserDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/UserDtoDao.java
@@ -3,6 +3,9 @@ package com.javamentor.qa.platform.dao.abstracts.dto;
 import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
 import com.javamentor.qa.platform.models.dto.UserDto;
 
+import java.util.Optional;
+
 public interface UserDtoDao extends ReadWriteDao<UserDto, Long> {
+    Optional<UserDto> getById(Long id);
 
 }
