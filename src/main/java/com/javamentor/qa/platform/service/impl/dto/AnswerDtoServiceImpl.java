ee472b79dfe7c5e224d9b76ddab073589de8faa0
Vlad Panin <vlad.frotello@gmail.com>
2023-04-26 05:50:10 +0000

    Resolve "Разработать api получения списка DTO ответов по id вопроса #18"

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/AnswerDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/AnswerDtoServiceImpl.java
@@ -1,9 +1,24 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
+import com.javamentor.qa.platform.dao.abstracts.dto.AnswerDtoDao;
+import com.javamentor.qa.platform.dao.impl.dto.AnswerDtoDaoImpl;
+import com.javamentor.qa.platform.models.dto.AnswerDto;
 import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnswerDtoServiceImpl implements AnswerDtoService {
 
+    private final AnswerDtoDao answerDtoDao;
+
+    public AnswerDtoServiceImpl(AnswerDtoDaoImpl answerDtoDao) {
+        this.answerDtoDao = answerDtoDao;
+    }
+
+    @Override
+    public List<AnswerDto> getAllAnswersDtoByQuestionId(Long questionId, Long userId) {
+        return answerDtoDao.getAllAnswersDtoByQuestionId(questionId,userId);
+    }
 }
--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,13 +1,17 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.models.dto.AnswerDto;
+import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
+import com.javamentor.qa.platform.service.impl.dto.AnswerDtoServiceImpl;
 import com.javamentor.qa.platform.service.impl.model.AnswerServiceImpl;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 
@@ -16,9 +20,11 @@ import java.util.NoSuchElementException;
 public class ResourceAnswerController {
 
     private final AnswerServiceImpl answerService;
+    private final AnswerDtoServiceImpl answerDtoService;
 
-    public ResourceAnswerController(AnswerServiceImpl answerService) {
+    public ResourceAnswerController(AnswerServiceImpl answerService, AnswerDtoServiceImpl answerDtoService) {
         this.answerService = answerService;
+        this.answerDtoService = answerDtoService;
     }
 
     @DeleteMapping("/{answerId}")
@@ -26,6 +32,22 @@ public class ResourceAnswerController {
         try {
             answerService.update(answerService.markDeleted(answerId));
             return new ResponseEntity<>("Answer was marked as deleted", HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>("Bad request", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @ApiOperation(value = "Get all answers by questionId", notes = "Retrieves a list of AnswerDto objects for a given questionId")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved the answers"),
+            @ApiResponse(code = 400, message = "Bad Request. Invalid questionId provided"),
+            @ApiResponse(code = 404, message = "Not Found. No answers found for the provided questionId")
+    })
+    @GetMapping
+    public ResponseEntity<?> getAllAnswers(@PathVariable Long questionId, Long userId) {
+        try {
+            List<AnswerDto> answersDto = answerDtoService.getAllAnswersDtoByQuestionId(questionId, userId);
+            return new ResponseEntity<>(answersDto, HttpStatus.OK);
         } catch (NoSuchElementException e) {
             return new ResponseEntity<>("Bad request", HttpStatus.BAD_REQUEST);
         }
