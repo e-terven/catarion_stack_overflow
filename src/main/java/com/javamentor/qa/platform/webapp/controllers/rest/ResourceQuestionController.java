52e66f9cd54e766be30f7b65d2ad77a075cd7baa
timur555@yandex.ru <rxl22jrn>
2023-05-02 13:42:31 +0300

    ref

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceQuestionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceQuestionController.java
@@ -1,4 +1,4 @@
-package com.javamentor.qa.platform.webapp.controllers;
+package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.converters.QuestionConverter;
 import com.javamentor.qa.platform.models.dto.QuestionCreateDto;
