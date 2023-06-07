6b330373796e5acf296f30558bd4d9ff75e8d8f5
Арман Калмахамбетов <arman458792@gmail.com>
2023-06-07 13:41:29 +0000

    Resolve "Разработать api которое возвращает DTO вопроса по Id #25"

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/QuestionDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/QuestionDtoService.java
@@ -1,4 +1,10 @@
 package com.javamentor.qa.platform.service.abstracts.dto;
 
+import com.javamentor.qa.platform.models.dto.QuestionDto;
+
+import java.util.Optional;
+import java.util.stream.DoubleStream;
+
 public interface QuestionDtoService {
+    Optional<QuestionDto> getById(Long questionId, long authorizedUserId);
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/dto/QuestionDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/QuestionDtoServiceImpl.java
@@ -1,9 +1,26 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
+import com.javamentor.qa.platform.dao.abstracts.dto.QuestionDtoDao;
+import com.javamentor.qa.platform.models.dto.QuestionDto;
 import com.javamentor.qa.platform.service.abstracts.dto.QuestionDtoService;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class QuestionDtoServiceImpl implements QuestionDtoService {
 
+    private final QuestionDtoDao questionDtoDao;
+
+    public QuestionDtoServiceImpl(QuestionDtoDao questionDtoDao) {
+        this.questionDtoDao = questionDtoDao;
+    }
+
+    @Override
+    @Transactional
+    public Optional<QuestionDto> getById(Long questionId, long authorizedUserId) {
+
+        return questionDtoDao.getById(questionId, authorizedUserId);
+    }
 }
